19e7cb77fd7d38b3a04810fbdf8a0f0ea085fd7e
ChristhianSM <christhian2524@gmail.com>
2024-01-22 15:12:06 -0500

    first commit

---
+++ src/task/task.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task } from 'src/schemas/task.schema';
+
+@Injectable()
+export class TaskService {
+  constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
+
+  finalAll() {
+    this.taskModel.find();
+  }
+
+  async create(createTask: any) {
+    const newTask = new this.taskModel(createTask);
+    await newTask.save();
+    return newTask;
+  }
+
+  async findOne(id: string) {
+    return this.taskModel.findById(id);
+  }
+
+  async delete(id: string) {
+    return this.taskModel.findByIdAndDelete(id);
+  }
+
+  async update(id: string, task: any) {
+    return this.taskModel.findByIdAndUpdate(id, task);
+  }
+}
